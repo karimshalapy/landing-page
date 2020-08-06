e05e84d857e864397f360a37cd1215044f19a845
Karim <karimshalapy@gmail.com>
2020-08-06 15:59:25 +0200

    feat: Added function to distinguish the section in view

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ const DOMObj = {
     mainSections: document.querySelectorAll("main section"),
     navBarList: document.getElementById("navbar__list")
 }
-
+let currentActiveSection;
 
 /**
  * End Global Variables
@@ -32,14 +32,33 @@ function navBuild() {
 
 
     //creating a document fragment for better 
-    let el = document.createDocumentFragment();
+    const el = document.createDocumentFragment();
     for (let i = 0; i < DOMObj.mainSections.length; i++) {
-        let current = DOMObj.mainSections[i];
-        let child = document.createElement("li");
+        const current = DOMObj.mainSections[i];
+        const child = document.createElement("li");
         child.innerHTML = `<a href="#${current.id}" class="menu__link"> ${current.dataset.nav} </a>`;
         el.appendChild(child);
     }
     DOMObj.navBarList.appendChild(el);
+}
+function isInView(el) {
+    const offsets = el.getBoundingClientRect();
+    return offsets.top <= window.innerHeight / 2 && offsets.bottom >= window.innerHeight / 2;
+}
+function setActiveStatus() {
+    const mainSectionsArr = Array.from(DOMObj.mainSections);
+    if (!currentActiveSection) {
+        currentActiveSection = mainSectionsArr[0]
+        currentActiveSection.classList.toggle("your-active-class")
+    }
+    for (let section of mainSectionsArr) {
+        if (isInView(section) && !(section === currentActiveSection)) {
+            currentActiveSection.classList.toggle("your-active-class")
+            currentActiveSection = section;
+            currentActiveSection.classList.toggle("your-active-class")
+            return currentActiveSection
+        }
+    }
 }
 
 
@@ -53,7 +72,7 @@ function main() {
     navBuild();
 
     // Add class 'active' to section when near top of viewport
-
+    setActiveStatus();
 
     // Scroll to anchor ID using scrollTO event
 
@@ -66,7 +85,12 @@ function main() {
  *
 */
 
-// Build menu 
+document.addEventListener("DOMContentLoaded", main)
+document.addEventListener("scroll", () => {
+    setActiveStatus();
+    // console.log(setActiveStatus());
+})
+// Build menu
 
 // Scroll to section on link click
 
